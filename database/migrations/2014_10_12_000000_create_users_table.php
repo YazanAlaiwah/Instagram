96962a79bdc08814d8719a9fa703c9772e6e1253
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2019-11-23 17:03:01 +0200

    make sample front-end

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,19 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->nullable();
             $table->string('email')->unique();
-            $table->string('paswword');
-            $table->string('user_name');
-            $table->string('name');
-            $table->integer('phon');
-            $table->longText('Bio');
-            $table->boolean('privet');	
-            $table->integer('posts');
-            $table->string('gender');	
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            // $table->rememberToken();
+            // $table->string('paswword');
+            $table->string('user_name')->nullable();
+            $table->string('name')->nullable();
+            $table->integer('phon')->nullable();
+            $table->longText('Bio')->nullable();
+            $table->boolean('privet')->nullable();	
+            $table->integer('posts')->nullable();
+            $table->string('gender')->nullable();	
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+  <div class="row">
+      <div class="col-3 p-5">
+          <img class="rounded-circle" src="https://instagram.famm7-1.fna.fbcdn.net/vp/f3a313f84e6ed16952fefe149eadbf43/5E85392C/t51.2885-19/s150x150/66631287_2267762349937646_4945189318428721152_n.jpg?_nc_ht=instagram.famm7-1.fna.fbcdn.net" alt="">
+      </div>
+      <div class="col-9 pt-5">
+        <div><h1>yazanalaiwah</h1></div>
+        <div class="d-flex">  
+          <div class="pr-5"><strong>78</strong>post</div>
+          <div class="pr-5"><strong>2k</strong>followers</div>
+          <div class="pr-5"><strong>58</strong>following</div>
         </div>
+        <div class="pt-5 font-weight-bold" >freecode.org</div>
+        <div>djskljfkl;djakfljeriojvklmckjnvkegmio</div>    
     </div>
+  </div>
+  <div class="row pt-5">
+      <div class="col-4">
+          <img class="w-100" src="https://instagram.famm6-1.fna.fbcdn.net/v/t51.2885-15/e35/73401917_2362539887340208_5006795626282830720_n.jpg?_nc_ht=instagram.famm6-1.fna.fbcdn.net&amp;_nc_cat=1&amp;oh=5f289d6bcc1bae49c3d7d98d649daa82&amp;oe=5E6AB423" alt="">
+      </div>
+      <div class="col-4">
+          <img class="w-100" src="https://instagram.famm6-1.fna.fbcdn.net/v/t51.2885-15/e35/72775890_2157969931164966_1188453808114236100_n.jpg?_nc_ht=instagram.famm6-1.fna.fbcdn.net&_nc_cat=106&oh=ae07afd813dceae3b15ee7ab59e05c29&oe=5E6EC265" alt="">
+      </div>
+      <div class="col-4">
+          <img class="w-100" src="https://instagram.famm6-1.fna.fbcdn.net/v/t51.2885-15/e35/72748364_159731945125275_8252885917152655796_n.jpg?_nc_ht=instagram.famm6-1.fna.fbcdn.net&amp;_nc_cat=1&amp;oh=52c3783724609ada305f74e6eb16dd71&amp;oe=5E76E941" alt="">
+      </div>
+  </div>
+    
 </div>
 @endsection
