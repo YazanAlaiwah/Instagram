5013bd5ecb1cb7ff4f172f6ee5451d929fd5934f
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2019-11-26 20:30:27 +0200

    bulid Post image front-end and back end

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,23 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function store()
+    {
+        $data = request()->validate([
+            'caption'=>'required',
+            'img'=>['required','image']
+        ]);
+        auth()->user()->posts()->create($data);
+        dd(request()->all());
+    }
+   
+
     public function create()
     {
         return view('posts.create');
--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
+
+    
+
     public function index($user)
     {
         $user = User::findOrFail($user);
